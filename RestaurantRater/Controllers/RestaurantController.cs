cedd1664676d7100c16152ebbde6f15177eaace4
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-03-25 12:43:12 -0400

    add view and controller methods for delete restaurant

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -36,5 +37,30 @@ namespace RestaurantRater.Controllers
             //When it isnt valid it goes back to the view with info you added so you can correct it
             return View(restaurant); 
         }
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)  //int? says (int id) can now be null
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        // POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete (int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
