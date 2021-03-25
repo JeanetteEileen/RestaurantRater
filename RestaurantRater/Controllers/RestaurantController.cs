17a8c0a6b67828f527f4e81b22e6ace22b21c61f
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-03-25 11:52:37 -0400

    add create view and create method in controller

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -17,5 +17,24 @@ namespace RestaurantRater.Controllers
             return View(_db.Restaurants.ToList());
             
         }
+        // GET: Restaurant/Create Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Restaurant/Create
+        [HttpPost] // Only a post method
+        [ValidateAntiForgeryToken] // verfies this is coming from right source
+        public ActionResult Create (Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //When it isnt valid it goes back to the view with info you added so you can correct it
+            return View(restaurant); 
+        }
     }
 }
